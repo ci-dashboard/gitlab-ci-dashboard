40bb1bab2f6ae49939347886c4df000e2409961d
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-11-08 22:37:03 -0200

    FIX: fixing e2e tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -12,7 +12,7 @@ module.exports = {
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       .assert.elementCount('img', 1)
-      .assert.elementPresent('#gcim-logo')
+      .assert.elementPresent('#gcim-painel')
       .assert.elementPresent('#gcim-error')
       .assert.elementPresent('#gcim-invalid-config')
       .assert.elementPresent('#gcim-loading')
