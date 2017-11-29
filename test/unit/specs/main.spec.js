a1cb0900e13b7fcb3c3ead9fd6ae60c42bf36ce3
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-11-29 04:11:07 -0200

    CHANGE: add main.js tests

--- test/unit/specs/main.spec.js
+++ test/unit/specs/main.spec.js
@@ -1,10 +1,44 @@
-import {
+import RootMain, {
   getParameterByName,
-  getProjectsByQuerystring,
   getTopItem
 } from '@/main.js'
 
+const mockedBuilds = [
+  {
+    id: 1,
+    status: 'running'
+  },
+  {
+    id: 3,
+    status: 'success'
+  },
+  {
+    id: 4,
+    status: 'failed'
+  },
+  {
+    id: 2,
+    status: 'success'
+  }
+]
+
 describe('main.js', () => {
+  describe('Sorted Builds', () => {
+    const vm = RootMain.$mount()
+    it('Should sort the builds by id from highest to lowest', () => {
+      vm.onBuilds = mockedBuilds
+      const arr = vm.sortedBuilds
+      expect(arr[0].id).toEqual(4)
+      expect(arr[3].id).toEqual(1)
+    })
+    it('Should sort the builds by id from highest to lowest without success cards', () => {
+      vm.onBuilds = mockedBuilds
+      vm.hideSuccessCards = true
+      const arr = vm.sortedBuilds
+      expect(arr[0].id).toEqual(4)
+      expect(arr[1].id).toEqual(1)
+    })
+  })
   describe('Utilities', () => {
     it('Should get top item of array', () => {
       const arr = ['a', 'b', 'c']
