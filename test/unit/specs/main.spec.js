ea75c5a01e14192a900a5a7614c3014d64fca710
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-11-29 04:41:06 -0200

    WIP: refactory main.js to utils

--- test/unit/specs/main.spec.js
+++ test/unit/specs/main.spec.js
@@ -1,7 +1,17 @@
-import RootMain, {
-  getParameterByName,
-  getTopItem
-} from '@/main.js'
+import RootMain from '@/main.js'
+
+jest.mock('@/standalone', () => ({
+  getStandaloneParams: () => {
+    return Promise.resolve({
+      gitlab: 'gitlab.com'
+    })
+  }
+}))
+jest.mock('@/utils', () => ({
+  getParameterByName: () => {
+    return 'true'
+  }
+}))
 
 const mockedBuilds = [
   {
@@ -38,45 +48,15 @@ describe('main.js', () => {
       expect(arr[0].id).toEqual(4)
       expect(arr[1].id).toEqual(1)
     })
-  })
-  describe('Utilities', () => {
-    it('Should get top item of array', () => {
-      const arr = ['a', 'b', 'c']
-      const topItem = getTopItem(arr)
-      expect('a').toEqual(topItem)
+    it('Should return empty sortedBuilds when builds is empty', () => {
+      vm.onBuilds = []
+      const arr = vm.sortedBuilds
+      expect(arr.length).toEqual(0)
     })
-  })
-  describe('Get querystring params', () => {
-    const url = 'http://localhost:8080/?gitlab=localhost:8089&projects=namespace1/project1/branch1&token=_&projectsFile=http://localhost:8080/static/file.json&gitlabciProtocol=http&interval=5&hideSuccessCards=false'
-    it('Should get non-optional params from url', () => {
-      // gitlab
-      const gitlab = getParameterByName('gitlab', url)
-      expect('localhost:8089').toEqual(gitlab)
-
-      // token
-      const token = getParameterByName('token', url)
-      expect('_').toEqual(token)
-
-      // projectsFile
-      const projectsFile = getParameterByName('projectsFile', url)
-      expect('http://localhost:8080/static/file.json').toEqual(projectsFile)
-
-      // projects
-      const projects = getParameterByName('projects', url)
-      expect('namespace1/project1/branch1').toEqual(projects)
-    })
-    it('Should get optional params from url', () => {
-      // gitlabciProtocol
-      const gitlabciProtocol = getParameterByName('gitlabciProtocol', url)
-      expect('http').toEqual(gitlabciProtocol)
-
-      // hideSuccessCards
-      const hideSuccessCards = getParameterByName('hideSuccessCards', url)
-      expect(false).toEqual(hideSuccessCards)
-
-      // interval
-      const interval = getParameterByName('interval', url)
-      expect('5').toEqual(interval)
+    describe('Standalone', () => {
+      it('Should load params from stadalone mode', () => {
+        expect(vm.projectsFile).toEqual('standalone')
+      })
     })
   })
 })
