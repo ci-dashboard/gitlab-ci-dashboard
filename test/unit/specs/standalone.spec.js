917a1d7c7e4116b61d7424695e0e35c1d3e8620f
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-12-10 02:30:54 -0200

    FIX: fixing standalone tests

--- test/unit/specs/standalone.spec.js
+++ test/unit/specs/standalone.spec.js
@@ -2,7 +2,7 @@ import {
   getStandaloneParams
 } from '@/standalone'
 
-jest.mock('axios', () => ({
+jest.mock('fitch', () => ({
   get: () => {
     return Promise.resolve({data: {
       gitlab: 'gitlab.example.com',
@@ -14,7 +14,7 @@ jest.mock('axios', () => ({
 
 describe('Standalone', () => {
   it('should get valid params for standalone mode', (done) => {
-    getStandaloneParams().then((params) => {
+    getStandaloneParams('http://example.gitlab.com').then((params) => {
       expect('gitlab.example.com').toEqual(params.gitlab)
       expect('12345').toEqual(params.token)
       expect('standalone').toEqual(params.projectsFile)
