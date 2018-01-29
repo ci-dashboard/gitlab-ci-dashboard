363ba21863b88930e852df3a95617e09064c4bc9
Emiliano Barbosa <emilianoeloi@gmail.com>
2018-01-29 21:07:52 -0200

    FIX: Invalid informations and options

--- src/components/InvalidConfig.vue
+++ src/components/InvalidConfig.vue
@@ -10,15 +10,34 @@
           The following properties must be defined in the URL.
         </p>
         <p>
-          Check <a href="https://github.com/emilianoeloi/gitlab-ci-dashboard/blob/master/README.md" target="_blank">README</a> for more config options.
+          Check [README](https://github.com/emilianoeloi/gitlab-ci-dashboard/blob/master/README.md) for more configuration options.
         </p>
         <ul>
           <li>gitlab</li>
           <li>token</li>
-          <li>projects</li>
+          <li>projectsFile</li>
+          <li>gitlabciProtocol (optional)</li>
+          <li>hideSuccessCards (optional)</li>
+          <li>hideVersion (optional)</li>
+          <li>interval (optional)</li>
+          <li>apiVersion (optional)</li>
         </ul>
+        <p>projectsFile json pattern</p>
+        <pre>
+[
+  {
+    "description": "React Native render for draft.js model",
+    "namespace": "globocom",
+    "project": "react-native-draftjs-render",
+    "branch": "develop"
+  }
+  (...)
+]
+        </pre>
       </div>
       <div class="ui bottom attached warning message">
+        <p>Ex: https://emilianoeloi.github.io/gitlab-ci-dashboard/?gitlab=gitlab.com&token=w_zZcGZw-8pKxX_qSbWy&projectsFile=https://emilianoeloi.github.io/gitlab-ci-dashboard/static/gitlab.json</p>
+        <h4>gitlab-ci-monitor compatibility</h4>
         <p>Ex: https://emilianoeloi.github.io/gitlab-ci-dashboard/?gitlab=gitlab.example.com&token=12345&projects=namespace/project1,namespace/project1/branch,namespace/project2</p>
       </div>
     </div>
